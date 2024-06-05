c02dc1e88fb91ed66db5d51fff8c4162d8d5b81b
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-06-05 16:12:02 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-playground/",
-  build: {
-    outDir: "./dist",
-  },
-});
-
+})
